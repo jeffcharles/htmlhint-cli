e20561d218e6b75f20bf92332a3064e3b273008e
mundi morgado <mundi@tandemseven.com>
2015-02-11 19:26:20 -0800

    add support for file extension

--- htmlhint-cli.js
+++ htmlhint-cli.js
@@ -7,12 +7,12 @@ module.exports = function(){
   var HTMLHint = require('htmlhint').HTMLHint;
 
   var args = require('minimist')(process.argv.slice(2));
-
-  glob.sync(args.html+'/**/*.html').forEach(function(file){
+  var ext = args.ext || 'html';
+  glob.sync(args.html + '/**/*.' + ext).forEach(function(file){
     var source = fs.readFileSync(file, 'utf-8');
     var report = HTMLHint.verify(source);
     if(report.length){
-      console.log('HTMLHint errors in', file);
+      console.log('HTML errors in', file);
       report.forEach(function(message){
         console.log('line', message.line+',', message.type+':', message.message);
       });
