6b675a9dd2741420dfd4149283946e2da877653e
mundi morgado <mundi@tandemseven.com>
2015-04-14 16:39:46 -0700

    support for rc file

--- htmlhint-cli.js
+++ htmlhint-cli.js
@@ -6,11 +6,28 @@ module.exports = function(){
   var glob = require('glob-all');
   var HTMLHint = require('htmlhint').HTMLHint;
 
+  var rules = {
+    "tagname-lowercase": true,
+    "attr-lowercase": true,
+    "attr-value-double-quotes": true,
+    "doctype-first": true,
+    "tag-pair": true,
+    "spec-char-escape": true,
+    "id-unique": true,
+    "src-not-empty": true,
+    "attr-no-duplication": true
+  }
+
+  var rulesPath = process.cwd() + '/.htmlhintrc';
+  if(fs.existsSync(rulesPath)){
+    rules = JSON.parse(fs.readFileSync(rulesPath, 'utf-8'));
+  }
+
   var args = require('minimist')(process.argv.slice(2));
   var ext = args.ext || 'html';
   glob.sync(args.html + '/**/*.' + ext).forEach(function(file){
     var source = fs.readFileSync(file, 'utf-8');
-    var report = HTMLHint.verify(source);
+    var report = HTMLHint.verify(source, rules);
     if(report.length){
       console.log('HTML errors in', file);
       report.forEach(function(message){
